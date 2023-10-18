67520fdc192b5d2a43bcae630cec2c93ce99e9ae
shiamsharif <shiam.sharif.07@gmail.com>
2023-10-18 16:34:44 +0600

    one loop

--- ProblemSolving/OneLoop/main.cpp
+++ ProblemSolving/OneLoop/main.cpp
@@ -3,21 +3,25 @@ using namespace std;
 
 int main()
 {
-
-    int n;
+    //*number of row n (user i/p) .
+    int n,m; 
     cout<<"Number of row : ";
     cin >>n;
+    // //*number of colum m (user i/p) .
+    // cout<<"Number of Colum : ";
+    // cin >>m;
     for(int i = 1; i<=n; i++)
     {
-        if(i%2==1)
+        if(i%2==1) //if line number is odd
         {
             cout << "Row : " << i << "\t\t";
-            cout<<((5*i)-4) <<"\t"<<((5*i)-3) <<"\t" <<((5*i)-2) <<"\t" <<((5*i)-1) <<"\t" <<((5*i)-0)<<endl; 
+            //!here i used 3 because one row contain three digit if it,s four or any number then i used here 4 or any number.
+            cout<<((3*i)-2) <<"\t" <<((3*i)-1) <<"\t" <<((3*i)-0)<<endl; 
         }
-        else
+        else // even
         {
             cout << "Row : " << i << "\t\t";
-           cout<<((5*i)-0) <<"\t"<<((5*i)-1) <<"\t" <<((5*i)-2) <<"\t" <<((5*i)-3) <<"\t" <<((5*i)-4)<<endl;
+           cout<<((3*i)-0) <<"\t"<<((3*i)-1) <<"\t" <<((3*i)-2)<<endl;
         }
     }
 }
